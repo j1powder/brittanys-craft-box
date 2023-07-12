b7841d2cfeea7f147877b506e5c123eee5315fc3
j1powder <j1powder@yahoo.com>
2023-07-11 20:08:11 -0700

    start of project

---
+++ layout/Header.js
@@ -0,0 +1,33 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container'
+import  NavDropdown  from 'react-bootstrap/NavDropdown';
+import classes from './Layouts.module.css'
+
+const Header = () => {
+
+    return <>
+        <Navbar expand="sm" className={classes.navmenu} >
+      <Container fluid>
+        <Navbar.Brand href="/">Brittany's Craft Box</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse >
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Products</Nav.Link>
+            <Nav.Link href="#action2">About</Nav.Link>
+            <Nav.Link href="#action2">Contact</Nav.Link>
+
+          </Nav>
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+}
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SSRProvider from 'react-bootstrap/SSRProvider';
+
+
+
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Arima&display=swap" rel="stylesheet" />
+  <SSRProvider>
+  <Component {...pageProps} />
+  </SSRProvider>
+  </>
 }
