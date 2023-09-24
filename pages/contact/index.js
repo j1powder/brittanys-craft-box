bb5dbace11c1c68f6287b7292e48443058f58080
j1powder <j1powder@yahoo.com>
2023-09-24 10:27:21 -0700

    still working on this

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -9,19 +9,49 @@ import Footer from '@/layout/Footer'
 import classes from '../../styles/Products.module.css'
 import Image from 'next/image'
 import farmermarket2 from '../../assets/farmersmarketpic1.jpg';
-
+import lascruces from '../../assets/LasCrucesView.jpg'
 
 const Contact = () => {
   
   
   return <>
   <Header/>
-  <main>
+  <main className={classes.page}>
 <Container>
     <Row>
         <Col md={12}>
             <Image className={classes.contactimage} src={farmermarket2} />
         </Col>
+    </Row>
+    <Row>
+      <Col md={12}>
+        <h2 className={classes.contactpage}>Brittany's Craft Box</h2>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={12}>
+        <h2 className={classes.contactpage}>575-640-8747</h2>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={12}>
+        <h2 className={classes.contactpage}>frombrittscrafts@gmail.com</h2>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={12}>
+        <h2 className={classes.contactpage}>Located in Las Cruces, NM</h2>
+      </Col>
+    </Row>
+    <Row>
+        <Col md={12}>
+            <Image className={classes.contactimage} src={lascruces} />
+        </Col>
+    </Row>
+    <Row>
+      <Col md={12}>
+        <h2 className={classes.contactpage}>Find us on social media</h2>
+      </Col>
     </Row>
     
 </Container>
